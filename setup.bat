08ae43fda9d1acb7bd274821b9a2a6a00187b89e
AngelParadaP <paradapangel@gmail.com>
2024-11-06 08:27:13 -0600

    setup

---
+++ setup.bat
@@ -0,0 +1,8 @@
+git config --global user.email "paradapangel@gmail.com"
+git config --global user.name "AngelPP"
+git config --global credential.helper store
+git config --global push.default simple
+git config --global http.postBuffer 524288000
+git config --global core.compression 0
+git config --globar core.longpaths true
+PAUSE
